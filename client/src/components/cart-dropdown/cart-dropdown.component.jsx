363bc2c20d7d178ff44a4b84d84491393c20b9fe
thiagodesantos <thiagodesantos23@gmail.com>
2021-10-02 16:30:22 -0300

    seperando o front do server e criando o back para o pagamento do stripe

---
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () =>  {
+    const cartItems = useSelector(selectCartItems);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    return (
+        <div className='cart-dropdown'>
+            <div className='cart-items'>
+            {
+                cartItems.length?(
+                    cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                    ))
+                ) : (
+                    <span className="empty-message">Seu carrinho está vazio</span>
+                )
+            }
+            </div>
+            <CustomButton onClick={() => {
+                    history.push("/checkout");
+                    dispatch(toggleCartHidden());
+                }}>Finalizar Compra</CustomButton>
+        </div>
+    );
+};
+  
+export default CartDropdown;
--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51JSMFTHoxBoQi7RTUxywdBVd95QHeTa9PS6mj4bkPb3NK88dHFzpGDB3gdOBVAcL8mXHxWAoIDIDEsN6Bhus6HNy00pM0Sw2oA';
 
     const onToken = token => {
-        alert('Pago com Sucesso')
-    }
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+            amount: priceForStripe,
+            token: token
+            }
+        })
+        .then(response => {
+            alert('Pagamento de teste realizado com sucesso');
+        })
+        .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+                'Ocorreu um erro durante o processo de pagamento de teste. Por favor verifique os dados informados.'
+            );
+        });
+    };
 
     return (
         <StripeCheckout 
